bf55fbc0f8991f21d040346699bacb50aa3621c0
madha <madhav@neuris.org>
2021-02-02 17:05:02 +0530

    basic policy gradient algorithm

--- basic_drl_algorithms/simple_policy_gradient.py
+++ basic_drl_algorithms/simple_policy_gradient.py
@@ -16,6 +16,7 @@ def mlp(sizes, activation=nn.Tanh, output_activation=nn.Identity):
     return nn.Sequential(*layers)
 
 
+# Training Function
 def train(env_name='CartPole-v0', hidden_sizes=[32], lr=1e-2, epochs=50, batch_size=5000, render=False):
     env = gym.make(env_name)
     assert isinstance(env.observation_space, Box), \
@@ -26,16 +27,17 @@ def train(env_name='CartPole-v0', hidden_sizes=[32], lr=1e-2, epochs=50, batch_s
     obs_dim = env.observation_space.shape[0]
     n_acts = env.action_space.n
 
-    # Make the core of the policy network
+    # Create the policy network i.e. network that learns the policy
     logits_net = mlp(sizes=[obs_dim] + hidden_sizes + [n_acts])
 
     # Make a function to compute the action distribution
-    # Passing the state to the policy network and getting the action distribution as an output
+    # For calculating action distribution, pass a state to the policy network
     def get_policy(obs):
         logits = logits_net(obs)
         return Categorical(logits=logits)
 
     # Make a function to select an action sampled from policy
+    # To get the action int sample an action out of the action distribution
     def get_actions(obs):
         return get_policy(obs).sample().item()
 
@@ -47,70 +49,71 @@ def train(env_name='CartPole-v0', hidden_sizes=[32], lr=1e-2, epochs=50, batch_s
     # Make the optimizer
     optimizer = Adam(logits_net.parameters(), lr=lr)
 
-    # For training policy
+    # For training the policy network
     def train_one_epoch():
-        batch_obs = []              # for observation
-        batch_acts = []             # for actions
-        batch_weights = []          # for R(tau) weighting in policy gradient
-        batch_rets = []             # for measuring episode returns
-        batch_lens = []             # for measuring episode lengths
+        batch_obs = []  # for observation
+        batch_acts = []  # for actions
+        batch_weights = []  # for R(tau) weighting in policy gradient
+        batch_rets = []  # for measuring episode returns
+        batch_lens = []  # for measuring episode lengths
 
         # Reset episode specific variables
-        obs = env.reset()           # first observation comes from starting distribution
-        done = False                # signal from environment that the episode is over
-        ep_rews = []                # list of rewards acquired throughout episode
+        obs = env.reset()  # first observation comes from starting distribution
+        done = False  # signal from environment that the episode is over
+        ep_rews = []  # list of rewards acquired throughout episode
 
         # render first episode of each epoch
         finished_rendering_this_epoch = False
 
         while True:
 
-            # rendering
+            # Render the environment
             if (not finished_rendering_this_epoch) and render:
                 env.render()
 
-            # save observations
+            # Save the observations
             batch_obs.append(obs.copy())
 
-            # act in the environment
+            # Take an action in the environment
             act = get_actions(torch.as_tensor(obs, dtype=torch.float32))
             obs, rew, done, _ = env.step(act)
 
-            # Save action and the reward
+            # Save the action and the reward
             batch_acts.append(act)
             ep_rews.append(rew)
 
             if done:
-                # if episode is over record information about the episode
+                # If the episode is complete, save all the information about the episode
                 ep_ret, ep_len = sum(ep_rews), len(ep_rews)
                 batch_rets.append(ep_ret)
                 batch_lens.append(ep_len)
 
-                # the weight for earch log probablity is R(tau)
+                # The weight for each log probability is the return
+                # i.e. for each logprob(a|s) weight is R(tau)
                 batch_weights += [ep_ret] * ep_len
 
-                # reset the episode specific variables
+                # Reset the episode specific variables
                 obs, done, ep_rews = env.reset(), False, []
 
-                # won't render this epoch again
+                # Stop rendering this epoch
                 finished_rendering_this_epoch = True
 
-                # end experience loop if we have enough of it
+                # End experience loop once you have enough samples
                 if len(batch_obs) > batch_size:
                     break
 
-        # take a single policy update step
+        # Taking a single policy network update step
         optimizer.zero_grad()
         batch_loss = compute_loss(
             obs=torch.as_tensor(batch_obs, dtype=torch.float32),
             act=torch.as_tensor(batch_acts, dtype=torch.float32),
             weights=torch.as_tensor(batch_weights, dtype=torch.float32)
-                                  )
+        )
         batch_loss.backward()
         optimizer.step()
         return batch_loss, batch_rets, batch_lens
 
-    # training loop
+    # Training Loop
     for i in range(epochs):
         batch_loss, batch_rets, batch_lens = train_one_epoch()
         print('epoch: %3d \t loss: %.3f \t return: %.3f \t ep_len: %.3f' %
@@ -119,6 +122,7 @@ def train(env_name='CartPole-v0', hidden_sizes=[32], lr=1e-2, epochs=50, batch_s
 
 if __name__ == '__main__':
     import argparse
+
     parser = argparse.ArgumentParser()
     parser.add_argument('--env_name', '--env', type=str, default='CartPole-v0')
     parser.add_argument('--render', action='store_true')
